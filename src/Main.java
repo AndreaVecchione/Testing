dbe510ea1dd6642369aeade3195db97e9a438ec2
AndreaVecchione <164344505+AndreaVecchione@users.noreply.github.com>
2024-06-20 00:35:37 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,14 @@ public class Main {
 
 
     public static void main(String[] args) {
+        //Creo un oggetto che stamperà una data
         OffsetDateTime dateTime = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-
+//data scritta in lunghezza LONG
         String dateLong = dateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
         System.out.println("Grandezza lunga: " + dateLong);
 
     }
-
+//Metodi che andremo poi a testare nella classe test
     static String formatInLong(OffsetDateTime dateTime) {
         return dateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
     }
